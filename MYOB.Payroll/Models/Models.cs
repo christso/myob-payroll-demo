615467cfdab358535fd95abe3fe09955b782d948
Christopher Tso <christso@gmail.com>
2019-09-08 16:14:28 +1000

    initial commit

---
+++ MYOB.Payroll/Models/Models.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MYOB.Payroll.Models
+{
+    public class PayrollContext : DbContext
+    {
+        public PayrollContext(DbContextOptions<PayrollContext> options)
+            : base(options)
+        {
+
+        }
+    }
+
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal AnnualSalary { get; set; }
+        public decimal SuperRate { get; set; }
+
+    }
+}
